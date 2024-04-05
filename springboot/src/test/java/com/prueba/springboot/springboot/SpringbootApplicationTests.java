c01c351a6ee00e7861e52a46eb1539de8405da56
SirFalcom <yguim00@hotmail.com>
2024-04-05 09:05:58 -0500

    inicio de la prueba

---
+++ springboot/src/test/java/com/prueba/springboot/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.springboot.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
